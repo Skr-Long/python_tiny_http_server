0f5bec9c718d065deebb807374001fd67c06032b
Skr-Long <1419196735@qq.com>
2024-05-03 21:02:26 +0800

    v1.1 通过读取文件的方式返回HTML

--- server/server_main.py
+++ server/server_main.py
@@ -1,26 +1,56 @@
 import socket
 import logging
 
-http_context = "<html><body><h1>Hello Server!</h1></body></html>"
+server_recv_len = 2048
 
-context_len = len(http_context)
-http_head = (
-    f"HTTP/1.1 200 OK\r\n"
-    f"Content-Type: text/html\r\n"
-    f"Content-Length: {context_len}\r\n"
-    f"\r\n"
+HTTP_HEAD_TEMPLATE = (
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: text/html\r\n"
+    "Content-Length: {0}\r\n"
+    "\r\n"
 )
 
-def init_log(filename):
-    logging.basicConfig(filename=filename, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+def read_html(filepath):
+    context = ""
+    with open(filepath, "r") as html:
+        for line in html:
+            context += line
+    return context
 
+
+def init_log_config():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)  # 可以设置为 DEBUG、INFO、WARNING、ERROR 或 CRITICAL
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('[%(asctime)s] - [%(name)s] - [%(levelname)s] - [%(message)s]')
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+
+def process_request(client_socket, client_address):
+    logging.info(f"Connection from {client_address}.")
+
+    data = client_socket.recv(server_recv_len)
+    logging.info(f"{data}")
+    
+    try:
+        http_context = read_html("asset/index.html")
+        http_head = HTTP_HEAD_TEMPLATE.format(len(http_context))
+        msg = http_head + http_context
+        data = msg.encode()
+        client_socket.sendall(data)
+    except TypeError as type_error:
+        logging.warning("catch exception: %s", type_error)
+    finally:
+        client_socket.close()
 
 
 def tcp_server():
     server_ip = '0.0.0.0'
     server_port = 10034
     server_listen_num = 10
-    server_recv_len = 2048
+
     
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((server_ip, server_port))
@@ -29,22 +59,11 @@ def tcp_server():
         logging.info(f"Server listening on {server_ip}:{server_port}")
         while True:
             client_socket, client_address = server_socket.accept()
-            logging.info(f"Connection from {client_address}.")
+            process_request(client_socket, client_address)
 
-            data = client_socket.recv(server_recv_len)
-            logging.info(f"{data}")
-            
-            try:
-                msg = http_head + http_context
-                data = msg.encode()
-                client_socket.sendall(data)
-            except TypeError as type_error:
-                logging.warning("catch exception: %s", type_error)
-            finally:
-                client_socket.close()
 
 if __name__ == "__main__":
-    init_log("log/run.log")
+    init_log_config()
     tcp_server()
         
         
